2e088aba35f31f2e6c10df703522c9a445f019a5
Michael Drake <michael.drake@codethink.co.uk>
2022-01-17 11:55:35 +0000

    util: Add helper to get leaf part of string.

--- src/util/util.h
+++ src/util/util.h
@@ -11,4 +11,16 @@
 
 #define CDT_ARRAY_COUNT(_a) ((sizeof(_a)) / (sizeof(*_a)))
 
+static inline const char *str_get_leaf(const char *str)
+{
+	const char *slash;
+
+	slash = strrchr(str, '/');
+	if (slash != NULL) {
+		return slash + 1;
+	}
+
+	return str;
+}
+
 #endif
